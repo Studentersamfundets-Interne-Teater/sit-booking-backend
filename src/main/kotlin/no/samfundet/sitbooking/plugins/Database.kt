4fab678e4c6733fae5419fc8a424e50d68036930
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2021-11-15 22:43:59 +0100

    Connect to db with exposed

--- src/main/kotlin/no/samfundet/sitbooking/plugins/Database.kt
+++ src/main/kotlin/no/samfundet/sitbooking/plugins/Database.kt
@@ -1,2 +1,14 @@
 package no.samfundet.sitbooking.plugins
 
+import io.ktor.application.*
+import org.jetbrains.exposed.sql.*
+import org.postgresql.*
+
+fun Application.connectToDatabase() {
+    Database.connect(
+        "jdbc:postgresql://localhost:55000/sitbooking",
+        driver = "org.postgresql.Driver",
+        user = "sitbooking",
+        password = "secret"
+    )
+}
