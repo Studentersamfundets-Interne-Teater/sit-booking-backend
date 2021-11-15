c0bacef535f8130b6d7d6ab04a79ada6efc572d3
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2021-11-15 23:42:57 +0100

    Setup user DAO

--- src/main/kotlin/no/samfundet/sitbooking/plugins/Database.kt
+++ src/main/kotlin/no/samfundet/sitbooking/plugins/Database.kt
@@ -1,14 +1,18 @@
 package no.samfundet.sitbooking.plugins
 
 import io.ktor.application.*
+import no.samfundet.sitbooking.user.UserRepository
 import org.jetbrains.exposed.sql.*
-import org.postgresql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
-fun Application.connectToDatabase() {
+fun Application.setUpDatabase() {
     Database.connect(
         "jdbc:postgresql://localhost:55000/sitbooking",
         driver = "org.postgresql.Driver",
         user = "sitbooking",
         password = "secret"
     )
+    transaction {
+        SchemaUtils.create(UserRepository.UserTable)
+    }
 }
--- src/main/kotlin/no/samfundet/sitbooking/user/UserRepository.kt
+++ src/main/kotlin/no/samfundet/sitbooking/user/UserRepository.kt
@@ -1,6 +1,44 @@
 package no.samfundet.sitbooking.user
 
-class UserRepository {
-    fun getUserByUsername() {
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object UserRepository {
+    object UserTable : Table("user_account") {
+        val username: Column<String> = text("username")
+        val fullName: Column<String> = text("fullname")
+        val email: Column<String> = text("email")
+        val phone: Column<String> = text("phone")
+        override val primaryKey = PrimaryKey(username)
     }
+
+    fun getAllUsers(): List<User> {
+        return transaction {
+            UserTable.selectAll().map(::mapUser)
+        }
+    }
+
+    fun getByUsername(username: String): User? {
+        return transaction {
+            UserTable.select { UserTable.username eq username }.map(::mapUser).firstOrNull()
+        }
+    }
+
+    fun create(user: User): String {
+        return transaction {
+            UserTable.insert {
+                it[username] = user.userName
+                it[fullName] = user.fullName
+                it[email] = user.email
+                it[phone] = user.phone
+            } get UserTable.username
+        }
+    }
+
+    private fun mapUser(row: ResultRow) = User(
+        userName = row[UserTable.username],
+        fullName = row[UserTable.fullName],
+        email = row[UserTable.email],
+        phone = row[UserTable.phone]
+    )
 }
